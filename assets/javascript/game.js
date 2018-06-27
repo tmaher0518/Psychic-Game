a5c38fd7436d5d1f26bb55cb8c081c057748e58f
tmaher0518 <tmaher0518.github.io>
2018-06-27 18:55:54 -0500

    Psuedo Code for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// Need counter for wins 
+var wins = 0;
+
+//Need loss counter
+var losses = 0;
+
+//Keep track of user input 
+var userInput = [];
+
+//Set max number of guesses for user 
+var maxGuesses = 10;
+
+//Need to generate random letter
+
+
+//Keep track of user guesses
+
+
