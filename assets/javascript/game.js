063995e43013f0eb35be34c147057490b298308f
tmaher0518 <tmaher0518.github.io>
2018-06-27 20:00:57 -0500

    Updated game logic var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,30 @@ var losses = 0;
 var userInput = [];
 
 //Set max number of guesses for user 
-var maxGuesses = 10;
+var guessesLeft = 10;
 
-//Need to generate random letter
+//Need to generate random letter 
 
+//Make an array of random letters a e i 
+
+//Generate number from 0 to length of array ----> 0  1 2 for aei  then save 
+
+//handle user input if they guess correctly need to update win counter by 1 ----> reset game "new random letter and rs incorrect guesses"
+
+//if they guess incorrectly decrease guessesLeft and ----> keep track of the user guess 
 
 //Keep track of user guesses
 
+//Need to target wins tag to display your number of wins in HTML
+var winsTag = document.getElementById("wins");
+winsTag.innerHTML = wins;
 
+var lossesTag = document.getElementById("losses");
+lossesTag.innerHTML = losses;
+
+var guessesLeftTag = document.getElementById("guessesLeft");
+guessesLeftTag.innerHTML = guessesLeft;
+var randomLetters = ["a","e","i"];
+var random = Math.floor(Math.random() * 3);
+var randomLetter = randomLetters[random];
+console.log(randomLetter);
