78756a0df8ea05426af0cc38f43b0b1eb61f3137
tmaher0518 <tmaher0518.github.io>
2018-06-29 19:15:34 -0500

    second commmit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,6 @@
-// Need counter for wins 
+//Setting variables for wins, losses, guessesLeft, guessedSoFar, and ComputerChoices.  Created an array to hold the letters.
 var wins = 0;
-
-//Need loss counter
 var losses = 0;
-
-//Keep track of user input 
-var userInput = [];
-
-//Set max number of guesses for user 
-var guessesLeft = 10;
-
-//Need to generate random letter 
-
-//Make an array of random letters a e i 
-
-//Generate number from 0 to length of array ----> 0  1 2 for aei  then save 
-
-//handle user input if they guess correctly need to update win counter by 1 ----> reset game "new random letter and rs incorrect guesses"
-
-//if they guess incorrectly decrease guessesLeft and ----> keep track of the user guess 
-
-//Keep track of user guesses
-
-//Need to target wins tag to display your number of wins in HTML
-var winsTag = document.getElementById("wins");
-winsTag.innerHTML = wins;
-
-var lossesTag = document.getElementById("losses");
-lossesTag.innerHTML = losses;
-
-var guessesLeftTag = document.getElementById("guessesLeft");
-guessesLeftTag.innerHTML = guessesLeft;
-var randomLetters = ["a","e","i"];
-var random = Math.floor(Math.random() * 3);
-var randomLetter = randomLetters[random];
-console.log(randomLetter);
+var guessesLeft = 9;
+var guessesSoFar = []; // array to push user choices to
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
